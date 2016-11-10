51c942306b1462af2fad041925850ada0148feef
Jia Qian <isjia@163.com>
2016-11-10 22:40:47 +0800

    add get all posts

--- examples/react-express-api-demo/server/routes.js
+++ examples/react-express-api-demo/server/routes.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var Post = require('./models/post');
 
 module.exports = function(app){
-  app.get('/api', function(req, res){
-    res.json({message: 'get request'});
+  app.get('/posts', function(req, res){
+    Post.find().sort({'createdAt': -1}).exec(function(err, posts){
+      if (err) {
+        return res.status(500).json({error: err.message});
+      }
+      res.json({posts: posts})
+    });
   });
 
   app.post('/api', function(req, res){
